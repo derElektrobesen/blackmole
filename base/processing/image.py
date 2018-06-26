53e020501cedc92781439ae370a3b18be8db43d8
Pavel Berezhnoy <pberejnoy2005@gmail.com>
2018-06-27 00:25:13 +0300

    contours finding

--- base/processing/image.py
+++ base/processing/image.py
@@ -11,29 +11,35 @@ import skimage.color
 
 from skimage import exposure
 from skimage import feature
+from skimage import morphology
+from skimage import measure
+
+from scipy.ndimage.morphology import binary_fill_holes
 
 class MoleImage:
     def __init__(self, img):
         self._img = np.array(img)
 
+    def findEthalonContours(self, grayImg):
+        ethalon = exposure.adjust_log(grayImg)
+        ethalon = feature.canny(ethalon, low_threshold=0.15, high_threshold=0.15, sigma=10)
+        ethalon = binary_fill_holes(ethalon)
+
+        se = morphology.disk(1, int)
+        ethalon = morphology.binary_opening(ethalon, se)
+
+        contours = measure.find_contours(ethalon, 0.99) # countours coordinates will be returned here
+        logging.error(">>>>>>>>>>>>>>>>>>>>>>")
+        logging.error(">>>>>>>>>>>>>>>>>>>>>>")
+        logging.error(contours)
+        logging.error(">>>>>>>>>>>>>>>>>>>>>>")
+        logging.error(">>>>>>>>>>>>>>>>>>>>>>")
+
     def process(self):
-        res = skimage.color.rgb2gray(self._img)
-        res = exposure.adjust_log(res)
-        res = feature.canny(res, low_threshold=0.15, high_threshold=0.15, sigma=10).astype(int)
+        grayImg = skimage.color.rgb2gray(self._img)
+        self.findEthalonContours(grayImg)
+
+        res = grayImg > 0.7         # Convert image to Blak-White image with threshold 0.7
+        res = 1 - res.astype(int)   # Invert image
+
         return misc.toimage(res)
-
-    def read_chan(self, img, chan):
-        img = img.copy()
-        if chan != 'r':
-            img[:, :, 0] = 0
-        if chan != 'g':
-            img[:, :, 1] = 0
-        if chan != 'b':
-            img[:, :, 2] = 0
-
-        return img
-
-    #def im2double(self, img):
-    #    min_val = np.min(img.ravel())
-    #    max_val = np.max(img.ravel())
-    #    return (img.astype('float') - min_val) / (max_val - min_val)
